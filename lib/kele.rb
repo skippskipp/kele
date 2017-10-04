0daaafe04accc1d2bc5766ac951ce1073a14caef
skippskipp <Scott@Melanies-MacBook-Pro.local>
2017-10-03 21:11:15 -0400

    Adds json gem and get_me user retriever method

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -9,6 +10,11 @@ class Kele
     body = JSON.parse(response.body)
     raise "Error: #{body['message']}" if response.code != 200
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(bloc_api_url("users/me"), headers: {"authorization" => @auth_token})
+    @user_data = JSON.parse(response.body)
   end
 
   private
